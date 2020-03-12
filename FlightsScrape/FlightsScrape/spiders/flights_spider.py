d1bafaa23e6eb596bc81923a23c780d3c1367434
Ronen Zolicha <ronen1245@gmail.com>
2020-03-12 17:37:14 +0200

    Adding notes Try except on yield to avoid crash

--- FlightsScrape/FlightsScrape/spiders/flights_spider.py
+++ FlightsScrape/FlightsScrape/spiders/flights_spider.py
@@ -16,24 +16,53 @@ class FlightsSpider(scrapy.Spider):
         self.driver = webdriver.Chrome("../chromedriver.exe")
         # self.driver.implicitly_wait(20)
 
+    # How to parse the information from the page
     def parse(self, response):
         self.driver.get(response.url)
-        timeout = 3
+
+        # Making the driver to wait until the condition is met (Waiting for the table to load to click on the button
+        # to see all the flights)
+        element_present = expected_conditions.presence_of_all_elements_located((By.LINK_TEXT, 'כל הטיסות'))
+        WebDriverWait(self.driver, 10).until(element_present)
+        allFlightsButton = self.driver.find_element_by_link_text('כל הטיסות')
+        allFlightsButton.click()
+
+        # Making the driver to wait until the condition is met (waiting for the full table to load)
+        element_present = expected_conditions.presence_of_all_elements_located((By.LINK_TEXT, 'חזרה לתצוגת דפדוף'))
+        WebDriverWait(self.driver, 10).until(element_present)
+
+        # Making the driver to wait until the condition is met (waiting for the JS flights info table)
         element_present = expected_conditions.presence_of_all_elements_located((By.CLASS_NAME, 'even'))
-        WebDriverWait(self.driver, timeout).until(element_present)
+        WebDriverWait(self.driver, 10).until(element_present)
+
+        # Getting the HTML data from the page
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
+
+        # Getting the odd flights from the table
         oddFlights = soup.find_all("tr", {"class": "odd"})
+
+        # Getting the even flights from the table
         evenFlights = soup.find_all("tr", {"class": "even"})
+
+        # Removing the first item in the Resultset because its not a flight
         evenFlights.pop(0)
+
+        # Join the two result sets to get one result set to enumerate
         flights = oddFlights + evenFlights
 
+        i = 0
+        # Enumerate over the flights to extract the data
         for flight in flights:
-            yield {
-                'company': flight.find("img", {"class": "logoImg"})['alt'],
-                'flightNumber': flight.find("td", {"class": "FlightNum"}).get_text(),
-                'cameFrom': flight.find("td", {"class": "FlightFrom"}).get_text(),
-                'flightTime': flight.find("td", {"class": "FlightTime"}).get_text(),
-                'finalTime': flight.find("td", {"class": "finalTime"}).get_text(),
-                'terminalNumber': flight.find("td", {"class": "localTerminal"}).get_text(),
-                'status': flight.find("td", {"class": "status"}).get_text(),
-            }
+            i = i + 1
+            try:
+                yield {
+                    'company': flight.find("img", {"class": "logoImg"})['alt'],
+                    'flightNumber': flight.find("td", {"class": "FlightNum"}).get_text(),
+                    'cameFrom': flight.find("td", {"class": "FlightFrom"}).get_text(),
+                    'flightTime': flight.find("td", {"class": "FlightTime"}).get_text(),
+                    'finalTime': flight.find("td", {"class": "finalTime"}).get_text(),
+                    'terminalNumber': flight.find("td", {"class": "localTerminal"}).get_text(),
+                    'status': flight.find("td", {"class": "status"}).get_text(),
+                }
+            except:
+                print('Flight number ' + str(i) + ' got into an error')
