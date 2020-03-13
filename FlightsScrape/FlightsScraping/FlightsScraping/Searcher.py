0104ae1ff4d1f93bbdaedfd6c1dd4d1ddf131d2e
Ronen Zolicha <ronen1245@gmail.com>
2020-03-13 15:01:10 +0200

    Fix crash while no flights info available

--- FlightsScrape/FlightsScraping/FlightsScraping/Searcher.py
+++ FlightsScrape/FlightsScraping/FlightsScraping/Searcher.py
@@ -3,6 +3,7 @@ from pyjsonq import JsonQ
 
 def numbersToCategory(number):
     switcher = {
+        0: "exit",
         1: "company",
         2: "flightNumber",
         3: "cameFrom",
@@ -25,87 +26,112 @@ if __name__ == '__main__':
         print('5. Search by updated landing time')
         print('6. Search by landing terminal number')
         print('7. Search by flight status')
+        print('0. Exit')
 
         selectionNumber = input('Choose your searching category: ')
         selectionCategory = numbersToCategory(int(selectionNumber))
 
         if selectionCategory == "company":
             company = input('Choose the company you want to search for: ')
-            json = JsonQ('json/flights.json')
-            result = json.at('flights').where_contains('company', company).get()
-            if not result:
-                print("There are no flights from the company: " + company)
-            else:
-                for i in range(0, len(result)):
-                    print(result[i])
-            print("\n")
+            try:
+                json = JsonQ('json/flights.json')
+                result = json.at('flights').where_contains('company', company).get()
+                if not result:
+                    print("There are no flights from the company: " + company)
+                else:
+                    for i in range(0, len(result)):
+                        print(result[i])
+                print("\n")
+            except:
+                print("There is no flights information at the moment, please try again" + "\n")
+
 
         elif selectionCategory == "flightNumber":
             flightNumber = input('Choose the flight number you want to search for: ')
-            json = JsonQ('json/flights.json')
-            result = json.at('flights').where_contains('flightNumber', flightNumber).get()
-            if not result:
-                print("There are no flights with the flight number': " + flightNumber)
-            else:
-                for i in range(0, len(result)):
-                    print(result[i])
-            print("\n")
+            try:
+                json = JsonQ('json/flights.json')
+                result = json.at('flights').where_contains('flightNumber', flightNumber).get()
+                if not result:
+                    print("There are no flights with the flight number': " + flightNumber)
+                else:
+                    for i in range(0, len(result)):
+                        print(result[i])
+                print("\n")
+            except:
+                print("There is no flights information at the moment, please try again" + "\n")
 
         elif selectionCategory == "cameFrom":
             cameFrom = input('Choose the origin of the flight you want to search for: ')
-            json = JsonQ('json/flights.json')
-            result = json.at('flights').where_contains('cameFrom', cameFrom).get()
-            if not result:
-                print("There are no flights from this origin: " + cameFrom)
-            else:
-                for i in range(0, len(result)):
-                    print(result[i])
-            print("\n")
+            try:
+                json = JsonQ('json/flights.json')
+                result = json.at('flights').where_contains('cameFrom', cameFrom).get()
+                if not result:
+                    print("There are no flights from this origin: " + cameFrom)
+                else:
+                    for i in range(0, len(result)):
+                        print(result[i])
+                print("\n")
+            except:
+                print("There is no flights information at the moment, please try again" + "\n")
 
         elif selectionCategory == "flightTime":
             flightTime = input('Choose the original landing time you want to search for: ')
-            json = JsonQ('json/flights.json')
-            result = json.at('flights').where_contains('flightTime', flightTime).get()
-            if not result:
-                print("There are no flights with this original landing time: " + flightTime)
-            else:
-                for i in range(0, len(result)):
-                    print(result[i])
-            print("\n")
+            try:
+                json = JsonQ('json/flights.json')
+                result = json.at('flights').where_contains('flightTime', flightTime).get()
+                if not result:
+                    print("There are no flights with this original landing time: " + flightTime)
+                else:
+                    for i in range(0, len(result)):
+                        print(result[i])
+                print("\n")
+            except:
+                print("There is no flights information at the moment, please try again" + "\n")
 
         elif selectionCategory == "finalTime":
             finalTime = input('Choose the updated landing time you want to search for: ')
-            json = JsonQ('json/flights.json')
-            result = json.at('flights').where_contains('finalTime', finalTime).get()
-            if not result:
-                print("There are no flights that supposed to land at that time: " + finalTime)
-            else:
-                for i in range(0, len(result)):
-                    print(result[i])
-            print("\n")
+            try:
+                json = JsonQ('json/flights.json')
+                result = json.at('flights').where_contains('finalTime', finalTime).get()
+                if not result:
+                    print("There are no flights that supposed to land at that time: " + finalTime)
+                else:
+                    for i in range(0, len(result)):
+                        print(result[i])
+                print("\n")
+            except:
+                print("There is no flights information at the moment, please try again" + "\n")
 
         elif selectionCategory == "terminalNumber":
             terminalNumber = input('Choose the landing terminal number you want to search for: ')
-            json = JsonQ('json/flights.json')
-            result = json.at('flights').where_contains('terminalNumber', terminalNumber).get()
-            if not result:
-                print("There are no flights landing in this terminal: " + terminalNumber)
-            else:
-                for i in range(0, len(result)):
-                    print(result[i])
-            print("\n")
+            try:
+                json = JsonQ('json/flights.json')
+                result = json.at('flights').where_contains('terminalNumber', terminalNumber).get()
+                if not result:
+                    print("There are no flights landing in this terminal: " + terminalNumber)
+                else:
+                    for i in range(0, len(result)):
+                        print(result[i])
+                print("\n")
+            except:
+                print("There is no flights information at the moment, please try again" + "\n")
 
         elif selectionCategory == "status":
             status = input('Choose the status you want to search for: ')
-            json = JsonQ('json/flights.json')
-            result = json.at('flights').where('status', '=', status).get()
-            if not result:
-                print("There are no flights with this status: " + status)
-            else:
-                for i in range(0, len(result)):
-                    print(result[i])
-            print("\n")
+            try:
+                json = JsonQ('json/flights.json')
+                result = json.at('flights').where('status', '=', status).get()
+                if not result:
+                    print("There are no flights with this status: " + status)
+                else:
+                    for i in range(0, len(result)):
+                        print(result[i])
+                print("\n")
+            except:
+                print("There is no flights information at the moment, please try again" + "\n")
+
+        elif selectionCategory == "exit":
+            exit()
 
         else:
             print('You choose wrong category, please choose again' + "\n")
-
